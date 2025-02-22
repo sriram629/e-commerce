d48d387503b279c01b01fa7962c51a95c7b40f8a
sriram <Sri.ramakrishna@eficens.com>
2025-02-22 18:40:33 +0530

    layout

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'QuickKart'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'QuickKart is a fast and smart shopping platform. An Amazon clone built with Next.js and MongoDB.'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Shop Fast, Shop Smart'
